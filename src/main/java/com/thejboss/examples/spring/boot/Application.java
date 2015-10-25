f588baecdfe337af46eb07e8efd6256fe9815697
jbraathen <jbraathen@gmail.com>
2015-10-24 20:51:59 -0500

    Trying to get H2 console servlet working l...

--- src/main/java/com/thejboss/examples/spring/boot/Application.java
+++ src/main/java/com/thejboss/examples/spring/boot/Application.java
@@ -1,7 +1,10 @@
 package com.thejboss.examples.spring.boot;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,13 +12,12 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
-	/*
+
 	@Bean
 	public ServletRegistrationBean h2servletRegistration() {
-	    ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
-	    registration.addUrlMappings("/console/*");
-	    return registration;
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+		registration.addUrlMappings("/console/*");
+		registration.addInitParameter("webAllowOthers", "true");
+		return registration;
 	}
-	*/
 }
