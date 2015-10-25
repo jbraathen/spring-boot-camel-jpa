6ed91771357ceb1696e6951abc3fe16e52217222
jbraathen <jbraathen@gmail.com>
2015-10-24 20:29:30 -0500

    added property to add the h2 console

--- src/main/java/com/thejboss/examples/spring/boot/Application.java
+++ src/main/java/com/thejboss/examples/spring/boot/Application.java
@@ -1,10 +1,7 @@
 package com.thejboss.examples.spring.boot;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -13,10 +10,12 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 	
+	/*
 	@Bean
 	public ServletRegistrationBean h2servletRegistration() {
 	    ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
 	    registration.addUrlMappings("/console/*");
 	    return registration;
 	}
+	*/
 }
